5e0d7ad21bff7d4c12a04e3d03da630b862acc8a
JeUk Park <qkrwpdnr4531@naver.com>
2023-11-07 15:38:57 +0900

    Modify Wise_saying's content

--- src/main/java/com/ll/Wise_saying.java
+++ src/main/java/com/ll/Wise_saying.java
@@ -1,5 +1,13 @@
 package com.ll;
 
 public class Wise_saying {
+    int id;
+    String content;
+    String authorName;
 
+    public Wise_saying(int id, String content, String authorName){
+        this.id = id;
+        this.content = content;
+        this.authorName =authorName;
+    }
 }
